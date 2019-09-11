1e25c1954bb4859adc2a3c964fff8b60083a94d7
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-09-11 19:03:32 +0530

    Change the datatype of content field

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import TextField
 
-from sqlparse.tokens import Text
 
 # first add the blog-article
 
 
 class Article(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE, verbose_name="Author")
-    content = Text()
+    content = TextField()
     title = models.CharField(max_length=50, verbose_name="Title")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Creation Date")
     article_image = models.FileField(blank=True, null=True, verbose_name="Add Photo to Article")
