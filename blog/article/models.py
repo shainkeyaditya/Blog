fe7b44085183ef7c5c6c2114a413d4b9f7e00b25
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-09-11 18:48:33 +0530

    Add default method for python class __str__ for convert object into string

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
-# first add the blog-article
+
 from sqlparse.tokens import Text
+
+# first add the blog-article
 
 
 class Article(models.Model):
@@ -11,3 +13,6 @@ class Article(models.Model):
     title = models.CharField(max_length=50, verbose_name="Title")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Creation Date")
     article_image = models.FileField(blank=True, null=True, verbose_name="Add Photo to Article")
+
+    def __str__(self):
+        return self.title
