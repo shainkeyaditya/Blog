6db155c9fa49bf86b069f484b4442f0e1a7f2c8f
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-09-11 18:43:43 +0530

    Add Article model

--- blog/article/models.py
+++ blog/article/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 # first add the blog-article
+from sqlparse.tokens import Text
+
+
+class Article(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, verbose_name="Author")
+    content = Text()
+    title = models.CharField(max_length=50, verbose_name="Title")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Creation Date")
+    article_image = models.FileField(blank=True, null=True, verbose_name="Add Photo to Article")
