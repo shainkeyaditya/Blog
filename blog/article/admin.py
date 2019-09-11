c94494964d4f1fed7df9c2a3bc2a510f1d911a38
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-09-11 18:29:40 +0530

    Add by default files when create article app

---
+++ blog/article/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+# from blog.article.models import Tweet
+#
+#
+# @admin.register(Tweet)
+# class ResumeAdmin(admin.ModelAdmin):
+#     pass
---
+++ blog/article/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+# first add the blog-article
