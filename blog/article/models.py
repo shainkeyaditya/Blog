232e0542367b31324c20aca5ab16475e4b6924f2
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-09-12 23:18:49 +0530

    Add new model comment

--- blog/article/models.py
+++ blog/article/models.py
@@ -16,3 +16,13 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name="Article", related_name="comments")
+    comment_content = models.CharField(max_length=200, verbose_name="comment")
+    comment_user = models.CharField(max_length=50, verbose_name="Name")
+    comment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment_content
