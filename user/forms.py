8e779165443f23a4320fd397ff98d73d92b07725
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-09-25 12:02:16 +0530

    Add clean method and check password validate condition

--- user/forms.py
+++ user/forms.py
@@ -6,3 +6,16 @@ class RegisterForm(forms.Form):
     password = forms.CharField(max_length=22, label="Password", widget=forms.PasswordInput)
     confirm = forms.CharField(max_length=22, label="Verify Password", widget=forms.PasswordInput)
 
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        confirm = self.cleaned_data.get("confirm")
+
+        if password and confirm != confirm:
+            raise forms.ValidationError("Passwords do not match")
+
+        values = {
+            "username" : username,
+            "password" : password
+        }
+        return values
