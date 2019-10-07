ca75d21e223a4f1a06ff5b7e12e76b26fbe07fd4
Shainkey Aditya Jain <shainkeyjain30@gmail.com>
2019-10-07 16:10:50 +0530

    add routes for addarticle,article with pass id,updte,delete and comment

--- article/urls.py
+++ article/urls.py
@@ -18,6 +18,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('',views.articles, name="articles"),
-path('index/',views.index, name="index"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('addarticle/', views.addArticle, name="addarticle"),
+    path('article/<int:id>', views.detail, name="detail"),
+    path('update/<int:id>', views.updateArticle, name="update"),
+    path('delete/<int:id>', views.deleteArticle, name="delete"),
+    path('', views.articles, name="articles"),
+    path('comment/<int:id>', views.addComment, name="comment"),
 ]
